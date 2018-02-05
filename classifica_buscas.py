c54c53a40b06a6f4b2761d95a02915408753eaa8
Horácio Dias <horacio.dias@yahoo.com>
2018-02-05 21:56:46 -0200

    Finalizando o capítulo 4 detalhes sobre o algoritmo base para utilizar somente os dados de treino para ser justo

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -4,16 +4,12 @@ from collections import Counter
 df = pd.read_csv('buscas.csv')
 X_df = df[['home', 'busca', 'logado']]
 Y_df = df['comprou']
+
 Xdummies_df = pd.get_dummies(X_df)
 Ydummies_df = Y_df
 
 X = Xdummies_df.values
 Y = Ydummies_df.values
-
-acerto_base = max(Counter(Y).values())
-
-taxa_de_acerto_base = 100.0 * acerto_base / len(Y)
-print("Taxa de acerto base: %f" % (taxa_de_acerto_base))
 
 porcentagem_treino = 0.9
 
@@ -38,7 +34,21 @@ total_de_acertos = sum(acertos)
 total_de_elementos = len(teste_dados)
 
 taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
- 
+
 print("Taxa de acerto do algoritmo: %f" % (taxa_de_acerto))
 print(total_de_elementos)
+
+acerto_base = max(Counter(teste_marcacoes).values())
+
+
+taxa_de_acerto_base = 100.0 * acerto_base / len(teste_marcacoes)
+print("Taxa de acerto base: %f" % (taxa_de_acerto_base))
+
+
+
+
+
+
+ 
+
 
